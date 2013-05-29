d695d4e967d6e8c82f2778f3c71c362dd1ce53e6
Michael Kuhlen <mqk@astro.berkeley.edu>
2013-05-29 11:52:11 -0700

    Added field lines to the figure of the anisotropic conduction test.

--- test_problems/AnisoConduction/make_plots.py
+++ test_problems/AnisoConduction/make_plots.py
@@ -1,6 +1,6 @@
 from yt.mods import *
 
-for n in range(11,12):
+for n in [0,11]:
     
     pf = load("DD%04i/DD%04i" % (n,n))
 
@@ -20,8 +20,23 @@ for n in range(11,12):
     slice = SlicePlot(pf, 'z', "Temperature", width = (1.0, '1'), origin='domain', axes_unit=('1','1'))  #.save()
     #slice = SlicePlot(pf, 'z', "Temperature", width = (1.0, '1'), origin='domain', axes_unit=None)  #.save()
 
-    filename = "DD%04i" % n
 
-    slice.save(mpl_kwargs={'bbox_inches':'tight'}, name="%s_slice_Temperature_z.eps" % filename)
+    x = (dd['x'].reshape((256,256)))[:,0]
+    y = (dd['y'].reshape((256,256)))[0,:]
+    Bx = dd['Bx'].reshape((256,256))
+    By = dd['By'].reshape((256,256))
+    ax = slice.plots['Temperature'].axes
+
+    ## I found that I had to reverse Bx and By to get the correct
+    ## closed circles. Something to do with the plot's origin being in
+    ## the lower left?
+    streams = ax.streamplot(x,y,By,Bx,color=(1,1,1,0.5),linewidth=1.5,density=1)
+
+    ax.set_rasterization_zorder(5.0)
+    
+    ## filename = "DD%04i_slice_Temperature_z.png" % n
+    filename = "DD%04i_slice_Temperature_z.eps" % n
+
+    slice.save(mpl_kwargs={'bbox_inches':'tight'}, name=filename)
 
 # the end
