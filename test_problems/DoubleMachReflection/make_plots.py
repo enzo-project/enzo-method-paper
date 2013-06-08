a0535af6b9510437f13a22f9a843323cfe05c2b6
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-08 16:52:51 -0700

    Updating the DoubleMach figures.

--- test_problems/DoubleMachReflection/make_plots.py
+++ test_problems/DoubleMachReflection/make_plots.py
@@ -6,11 +6,12 @@ for n in range(11,12):
 
     c = [2.0,0.5,0]
 
-    buff = [120,480]  # this is backward because matplotlib transposes buffers.
+    slice = SlicePlot(pf, 'z', "Density", center=c, width=((4.15, '1'), (1.15, '1')),
+                      origin='domain', axes_unit=('1','1'))
 
-    slice = SlicePlot(pf, 'z', "Density", center=c, width=((4.0, '1'), (1.0, '1')), origin='domain', axes_unit=('1','1')) 
-    slice.set_buff_size(buff)
+    slice.set_zlim('Density',.5, 20)
 
-    filename = "DD%04i" % n
+    # See matplotlib issue #1188
+    slice.plots['Density'].cb.solids.set_edgecolor('face')
 
-    slice.save(mpl_kwargs={'bbox_inches':'tight'},name="%s_slice_Density_z.eps" % filename)
+    slice.save(mpl_kwargs={'bbox_inches':'tight'},name="DoubleMachTest.eps")
