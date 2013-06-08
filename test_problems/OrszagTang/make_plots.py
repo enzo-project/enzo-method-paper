a399b9b83a0fa0dd4ac65e81985e58f4a8c70125
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-08 16:31:41 -0700

    Updating the Orszag-Tang figures.

--- test_problems/OrszagTang/make_plots.py
+++ test_problems/OrszagTang/make_plots.py
@@ -3,9 +3,13 @@ name1 = './CT/DD0056/data0056'
 name2 = './Dedner/DD0032/data0032'
 pf1 = load(name1)
 pf2 = load(name2)
-names=['mhdct_0056','mhddedner_0035']
-sl=[]
+names=[
+    'MHDCT_OrszagTang_Density.eps',
+    'MHDDedner_OrszagTang_Density.eps'
+    ]
 for i, pf in enumerate([pf1,pf2]):
-  sl.append( SlicePlot( pf, 2, 'Density') )
-  sl[-1].set_zlim('Density',0.1,1)
-  print sl[-1].save(names[i]+".eps")
+  sl = SlicePlot( pf, 2, 'Density', origin='domain', width=(1.05, 'unitary'))
+  sl.set_zlim('Density',0.1,1)
+  # See matplotlib issue #1188
+  sl.plots['Density'].cb.solids.set_edgecolor('face')
+  print sl.save(names[i])
