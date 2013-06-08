fdecefa8c7b57515e435a5ee2114d04c18032395
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-08 16:03:30 -0700

    Adding .hgignore for test outputs and latex files.

--- test_problems/AnisoConduction/make_plots.py
+++ test_problems/AnisoConduction/make_plots.py
@@ -1,5 +1,8 @@
 from yt.mods import *
 
+filenames = {0: 'aniso_conduction_initial_output.eps',
+             11: 'aniso_conduction_final_output.eps'}
+
 for n in [0,11]:
     
     pf = load("DD%04i/DD%04i" % (n,n))
@@ -13,30 +16,15 @@ for n in [0,11]:
     print dd["Temperature"].min()
     print dd["Temperature"].max()
 
-    #print "Density:"
-    #print dd["Density"].min()
-    #print dd["Density"].max()
+    slice = SlicePlot(pf, 'z', "Temperature", width = (1.05, '1'), origin='domain', axes_unit=('1','1'))  #.save()
 
-    slice = SlicePlot(pf, 'z', "Temperature", width = (1.0, '1'), origin='domain', axes_unit=('1','1'))  #.save()
-    #slice = SlicePlot(pf, 'z', "Temperature", width = (1.0, '1'), origin='domain', axes_unit=None)  #.save()
-
-
-    x = (dd['x'].reshape((256,256)))[:,0]
-    y = (dd['y'].reshape((256,256)))[0,:]
-    Bx = dd['Bx'].reshape((256,256))
-    By = dd['By'].reshape((256,256))
-    ax = slice.plots['Temperature'].axes
-
-    ## I found that I had to reverse Bx and By to get the correct
-    ## closed circles. Something to do with the plot's origin being in
-    ## the lower left?
-    streams = ax.streamplot(x,y,By,Bx,color=(1,1,1,0.5),linewidth=1.5,density=1)
-
-    ax.set_rasterization_zorder(5.0)
+    slice.annotate_streamlines('Bx', 'By', plot_args={'color':(1,1,1,0.5),'linewidth':1.5})
     
-    ## filename = "DD%04i_slice_Temperature_z.png" % n
-    filename = "DD%04i_slice_Temperature_z.eps" % n
+    slice.set_zlim('Temperature', 7e5, 2e7)
 
-    slice.save(mpl_kwargs={'bbox_inches':'tight'}, name=filename)
+    # See matplotlib issue #1188
+    slice.plots['Temperature'].cb.solids.set_edgecolor('face')
+    
+    slice.save(name=filenames[n], mpl_kwargs={'bbox_inches':'tight'})
 
 # the end
