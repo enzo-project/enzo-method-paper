6d2bdbf2a438d6997079a316b1d1d735c7bb5872
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-09 14:15:37 -0700

    Small tweaks for the DoubleMachReflection figure.
    Adding ms.out to .hgignore as well.

--- test_problems/DoubleMachReflection/make_plots.py
+++ test_problems/DoubleMachReflection/make_plots.py
@@ -6,11 +6,16 @@ for n in range(11,12):
 
     c = [2.0,0.5,0]
 
-    slice = SlicePlot(pf, 'z', "Density", center=c, width=((4.15, '1'), (1.15, '1')),
+    slice = SlicePlot(pf, 'z', "Density", center=c,
+                      width=((4.15, '1'), (1.15, '1')),
                       origin='domain', axes_unit=('1','1'))
 
     slice.set_zlim('Density',.5, 20)
 
+    slice.set_buff_size((1000, 300))
+    
+    slice.set_window_size(12)
+    
     # See matplotlib issue #1188
     slice.plots['Density'].cb.solids.set_edgecolor('face')
 
