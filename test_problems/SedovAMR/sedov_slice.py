3bea570300fbf2c88be506c2ade48934651ceced
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-08 17:24:17 -0700

    Updating the sedov blast plots.

--- test_problems/SedovAMR/sedov_slice.py
+++ test_problems/SedovAMR/sedov_slice.py
@@ -4,9 +4,17 @@ from yt.mods import *
 
 fn1 = "PPM/DD0007/DD0007"
 pf1 = load(fn1)
-slice1 = SlicePlot(pf1, 'z', "Density", center=[0.5, 0.5, 0.0]).save("PPM_Density_slice.eps")
+slice1 = SlicePlot(pf1, 'z', "Density", center=[0.5, 0.5, 0.0], origin='domain')
+slice1.set_width((1.05,'unitary'))
+# See matplotlib issue #1188
+slice1.plots['Density'].cb.solids.set_edgecolor('face')
+slice1.save("sedov-ppm-slice.eps", mpl_kwargs={'bbox_inches':'tight'})
 
 fn2 = "Zeus/DD0007/DD0007"
 pf2 = load(fn2)
-slice2 = SlicePlot(pf2, 'z', "Density", center=[0.5, 0.5, 0.0]).save("Zeus_Density_slice.eps")
+slice2 = SlicePlot(pf2, 'z', "Density", center=[0.5, 0.5, 0.0], origin='domain')
+slice2.set_width((1.05,'unitary'))
+# See matplotlib issue #1188
+slice2.plots['Density'].cb.solids.set_edgecolor('face')
+slice2.save("sedov-zeus-slice.eps", mpl_kwargs={'bbox_inches':'tight'})
 
