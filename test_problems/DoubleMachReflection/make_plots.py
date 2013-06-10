0c00d577b97a04dcab134533b1c1c504c3d44a72
Nathan Goldbaum <goldbaum@ucolick.org>
2013-06-09 17:09:29 -0700

    Removing colorbar unit annotations from plots for unitless problems

--- test_problems/DoubleMachReflection/make_plots.py
+++ test_problems/DoubleMachReflection/make_plots.py
@@ -1,22 +1,28 @@
 from yt.mods import *
 
+def _DensityCode(field, data):
+    return data['Density']/data.convert('Density')
+
+add_field('DensityCode', display_name="Density", units="",
+          function=_DensityCode)
+
 for n in range(11,12):
 
     pf = load("DD%04i/DD%04i" % (n,n))
 
     c = [2.0,0.5,0]
 
-    slice = SlicePlot(pf, 'z', "Density", center=c,
+    slice = SlicePlot(pf, 'z', "DensityCode", center=c,
                       width=((4.15, '1'), (1.15, '1')),
                       origin='domain', axes_unit=('1','1'))
 
-    slice.set_zlim('Density',.5, 20)
+    slice.set_zlim('DensityCode',.5, 20)
 
     slice.set_buff_size((1000, 300))
     
     slice.set_window_size(12)
     
     # See matplotlib issue #1188
-    slice.plots['Density'].cb.solids.set_edgecolor('face')
+    slice.plots['DensityCode'].cb.solids.set_edgecolor('face')
 
     slice.save(mpl_kwargs={'bbox_inches':'tight'},name="DoubleMachTest.eps")
--- test_problems/OrszagTang/make_plots.py
+++ test_problems/OrszagTang/make_plots.py
@@ -1,4 +1,11 @@
 from yt.mods import *
+
+def _DensityCode(field, data):
+    return data['Density']/data.convert('Density')
+
+add_field('DensityCode', display_name="Density", units="",
+          function=_DensityCode)
+
 name1 = './CT/DD0056/data0056'
 name2 = './Dedner/DD0032/data0032'
 pf1 = load(name1)
@@ -8,9 +15,9 @@ names=[
     'MHDDedner_OrszagTang_Density.eps'
     ]
 for i, pf in enumerate([pf1,pf2]):
-  sl = SlicePlot( pf, 2, 'Density', origin='domain', width=(1.05, 'unitary'))
+  sl = SlicePlot( pf, 2, 'DensityCode', origin='domain', width=(1.05, 'unitary'))
   sl.set_window_size(5)
-  sl.set_zlim('Density',0.1,1)
+  sl.set_zlim('DensityCode',0.1,1)
   # See matplotlib issue #1188
-  sl.plots['Density'].cb.solids.set_edgecolor('face')
+  sl.plots['DensityCode'].cb.solids.set_edgecolor('face')
   print sl.save(names[i],mpl_kwargs={'bbox_inches':'tight'})
